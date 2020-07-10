f35638de6f0442f87f9df05f88279a80f29c8165
CloudUREE089471@cloudpc2c04.huawei.com <CloudUREE089471@cloudpc2c04.huawei.com>
2020-07-10 18:28:13 +0800

    report报表完成

--- src/components/content/Reports.vue
+++ src/components/content/Reports.vue
@@ -1,14 +1,47 @@
 <template>
-  <div>reports</div>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+    <div
+      id="main"
+      style="width: 750px;height:400px;"
+    ></div>
+  </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
-    name:"Reports"
+  name: "Reports",
+  data() {
+    return {
+      options: { title: { text: '用户来源' }, tooltip: { trigger: 'axis', axisPointer: { type: 'cross', label: { backgroundColor: '#E9EEF3' } } }, grid: { left: '3%', right: '4%', bottom: '3%', containLabel: true }, xAxis: [{ boundaryGap: false }], yAxis: [{ type: 'value' }] }
+    }
+  },
+  async mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
+
+    const { data: res } = await this.$http.get("reports/type/1");
+    if (res.meta.status !== 200) return this.$message.error("获取报表失败");
+    this.$message.success(res.meta.msg);
+
+    const result = Object.assign(this.options, res.data)
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result);
+
+  }
 
 }
 </script>
 
-<style>
-
+<style scoped>
+#main {
+  margin-top: 20px;
+}
 </style>
